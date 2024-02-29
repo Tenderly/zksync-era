f2a59d989beba59732e3962c2ca2ba6ac181dd37
NebojsaHorvat <horvatnebojsa@yahoo.com>
2024-02-29 09:53:47 +0100

    Crete init VM function which can be called by tenderly core

--- core/lib/tenderly/src/lib.rs
+++ core/lib/tenderly/src/lib.rs
@@ -1,6 +1,16 @@
-use zksync_multivm::VmInstance;
-use zksync_utils::{bytecode::hash_bytecode, u256_to_h256};
-
+use multivm::VmInstance;
+use zksync_utils::bytecode::hash_bytecode;
+use multivm::vm_latest::L1BatchEnv;
+use multivm::vm_latest::L2BlockEnv;
+use multivm::vm_latest::SystemEnv;
+use multivm::vm_latest::constants::BLOCK_GAS_LIMIT;
+use multivm::vm_latest::TxExecutionMode;
+use multivm::vm_latest::VmExecutionMode;
+use zksync_state::{InMemoryStorage, StorageView};
+use multivm::interface::VmInterface;
+use zksync_types::{L1BatchNumber,fee_model::BatchFeeInput, Address,block::MiniblockHasher, MiniblockNumber, ProtocolVersionId, L2ChainId};
+use zksync_contracts::BaseSystemContracts;
+use multivm::vm_latest::HistoryDisabled;
 fn default_l1_batch() -> L1BatchEnv {
     L1BatchEnv {
         previous_batch_hash: None,
@@ -10,6 +20,7 @@ fn default_l1_batch() -> L1BatchEnv {
             50_000_000_000, // 50 gwei
             250_000_000,    // 0.25 gwei
         ),
+
         fee_account: Address::random(),
         enforced_base_fee: None,
         first_l2_block: L2BlockEnv {
@@ -29,20 +40,22 @@ fn default_system_env() -> SystemEnv {
         gas_limit: BLOCK_GAS_LIMIT,
         execution_mode: TxExecutionMode::VerifyExecute,
         default_validation_computational_gas_limit: BLOCK_GAS_LIMIT,
-        chain_id: L2ChainId::from(270),
+        chain_id: L2ChainId::from(324),
     }
 }
 
 fn default_empty_storage() -> InMemoryStorage {
     InMemoryStorage::with_system_contracts(hash_bytecode)
 }
-
-pub fn init_vm() {
+#[no_mangle]
+pub extern "C" fn zksync_init_vm() {
+    println!("RUST FUNCTION IS SUCCESSFULLY CALLED!");
     let l1_batch_env = default_l1_batch();
     let system_env = default_system_env();
-    let mut raw_storage = default_empty_storage();
+    let raw_storage = default_empty_storage();
     let storage_ptr = StorageView::new(raw_storage).to_rc_ptr();
 
-    let vm = VmInstance::new(l1_batch_env, system_env, storage_ptr.clone());
+    let mut vm:VmInstance<StorageView<InMemoryStorage>, HistoryDisabled> = VmInstance::new(l1_batch_env, system_env, storage_ptr.clone());
     vm.execute(VmExecutionMode::OneTx);
+    println!("RUST ZKSYNC VM IS SUCCESSFULLY CALLED!");
 }
